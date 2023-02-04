28cf5b8e5dc1ab55fa1f3b176fd60ff7c5fa5e2c
bobrenja <abcd43@ukr.net>
2023-02-04 17:09:04 +0200

    books add hooks

--- src/components/PhoneBooks/MyPhoneBooks.jsx
+++ src/components/PhoneBooks/MyPhoneBooks.jsx
@@ -1,62 +1,39 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import MyForm from './Form/MyForm';
 import Contacts from './Contacts/Contacts';
 import Search from './Search/Search';
 
-class MyPhoneBooksForm extends Component {
-
-  static defaultProps = {};
-  static propTypes = {};
-
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
+const MyPhoneBooksForm = () => {
+  const [contacts, setContacts] = useState(() => {
     const contacts = JSON.parse(localStorage.getItem('contactPhone'));
-    console.log(contacts);
-    if (contacts && contacts.length) {
-      //contacts?.length
-      this.setState({ contacts });
-    }
-  }
+    return contacts ? contacts : [];
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
+  useEffect(() => {
+    localStorage.setItem('contactPhone', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (prevState.contacts.length !== contacts) {
-      localStorage.setItem('contactPhone', JSON.stringify(contacts));
-    }
-  }
-
-  handleEnterInput = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  addBook = ({ name, number }) => {
+  const addBook = ({ name, number }) => {
     const isName = Boolean(
-      this.state.contacts.find(e => name.toLowerCase() === e.name.toLowerCase())
+      contacts.find(e => name.toLowerCase() === e.name.toLowerCase())
     );
     if (isName) {
       return alert(`${name} is contact book`);
     }
 
-    this.setState(prevState => {
-      const { contacts } = prevState;
-      const newCont = { id: nanoid(), name: name, number: number };
-
-      return { contacts: [newCont, ...contacts], name: '', number: '' };
-    });
+    const newCont = { id: nanoid(), name: name, number: number };
+    setContacts(prevCont => [newCont, ...prevCont]);
   };
 
-  findContact() {
-    const { filter, contacts } = this.state;
+  const handleEnterInput = ({ target }) => {
+    const { value } = target;
+    setFilter(value);
+  };
+
+  function findContact() {
     if (!filter) {
       return contacts;
     }
@@ -69,29 +46,27 @@ class MyPhoneBooksForm extends Component {
     return res;
   }
 
-  deleteContact = id => {
-    const { contacts } = this.state;
+  const deleteContact = id => {
     const newContact = contacts.filter(e => e.id !== id);
-    this.setState({ contacts: newContact });
+    console.log(newContact);
+    setContacts(newContact);
   };
 
-  render() {
-    const contactsFilter = this.findContact();
-    const isContact = Boolean(contactsFilter.length);
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <MyForm onSubmit={this.addBook} />
-        <Search handleEnterInput={this.handleEnterInput} />
-        {isContact && (
-          <Contacts
-            contactsFilter={contactsFilter}
-            deleteContact={this.deleteContact}
-          />
-        )}
-      </>
-    );
-  }
-}
+  const isContact = Boolean(contacts.length);
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <MyForm onSubmit={addBook} />
+      <Search handleEnterInput={handleEnterInput} />
+      {isContact && (
+        <Contacts
+          contactsFilter={findContact()}
+          deleteContact={deleteContact}
+        />
+      )}
+    </>
+  );
+};
 
 export default MyPhoneBooksForm;
